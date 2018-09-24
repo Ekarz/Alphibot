b981096d7b295fac0f1da32f670b5a181b3bb7c9
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-24 21:53:20 +0200

    remove guildOnly status from recap command

--- commands/recap.js
+++ commands/recap.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'recap',
     args: false,
-    guildOnly: true,
     description: 'Demande un récapitulatif de l\'organisation actuelle pour le raid.',
     execute(message, args) {
         message.channel.send('Je ne sais pas encore lancer une dispo mais ça vient !')
