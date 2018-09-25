e370a3fbf6ea6acf71879bfc01702ec4d35e26c0
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-25 21:53:48 +0200

    add CDs

--- alfino.js
+++ alfino.js
@@ -4,6 +4,7 @@ const { token, prefix } = require('./properties.json')
 const { version }       = require('./package.json')
 const client            = new Discord.Client()
 client.commands         = new Discord.Collection()
+const cooldowns         = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 for (const file of commandFiles) {
@@ -31,6 +32,30 @@ client.on('message', message => {
     }
     if (command.officerOnly && !message.member.roles.some(role => role.name === 'Officiers')) {
         return message.reply('Je ne peux exécuter cette commande que si un officier me le demande. Désolé !')
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (!timestamps.has(message.author.id)) {
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    }
+    else {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.author.send(`Merci d'attendre encore ${timeLeft.toFixed(1)} seconde(s) avant de réutiliser la commande \`${command.name}\`.`);
+        }
+
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
     }
 
     try {
