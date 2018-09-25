ede9601a06933fb85f6576e92c8a1bc8e7bb6320
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-25 22:41:39 +0200

    add organizeRaid in recap command

--- commands/recap.js
+++ commands/recap.js
@@ -8,6 +8,7 @@ module.exports = {
     execute(message, args) {
         if (raid.length) {
             const data = []
+            const data2 = []
             for (raidDay of raid) {
                 if (Object.keys(raidDay.players).length) {
                     let str = `**${raidDay.date} :** `
@@ -16,8 +17,25 @@ module.exports = {
                     }
                     data.push(str.slice(0, -2))
                 }
+                const raidHour = organizeRaid(Object.values(raidDay.players))
+                if (raidHour !== null) {
+                    data2.push(`Il est possible de partir le **${raidDay.date}** à ${raidHour}`)
+                }
             }
-            data.length ? message.channel.send(data) : message.channel.send("Personne ne s'est inscrit-e pour l'instant...")
+            message.channel.send(data.length ? data : "Personne ne s'est inscrit-e pour l'instant...")
+            if (data2.length) message.channel.send(data2)
         } else return message.channel.send("Aucun raid n'a été préparé pour le moment !")
     },
 }
+
+const convertToMinutes = hour => {
+    const tab = hour.split(/[Hh:]/).map(e => Number(e))
+    return tab[0] * 60 + tab[1]
+}
+
+const convertToString = minutes => `${Math.floor(minutes / 60)}h${String(minutes % 60).padStart(2, '0')}`
+
+const findMax = array => array.reduce((acc, e) => acc = e > acc ? e : acc, array.pop())
+
+// finds earliest hour for now
+const organizeRaid = players => players.length >= 8 ? convertToString(findMax(players.map(e => convertToMinutes(e)))) : null
