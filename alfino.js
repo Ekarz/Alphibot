915c656ed65d31fd4e3c1df1ae6bd2e4052e1def
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-25 22:41:09 +0200

    reformat and change CD behavior

--- alfino.js
+++ alfino.js
@@ -1,10 +1,10 @@
-const Discord           = require('discord.js')
-const fs                = require('fs')
+const Discord = require('discord.js')
+const fs = require('fs')
 const { token, prefix } = require('./properties.json')
-const { version }       = require('./package.json')
-const client            = new Discord.Client()
-client.commands         = new Discord.Collection()
-const cooldowns         = new Discord.Collection();
+const { version } = require('./package.json')
+const client = new Discord.Client()
+client.commands = new Discord.Collection()
+const cooldowns = new Discord.Collection()
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 for (const file of commandFiles) {
@@ -35,27 +35,27 @@ client.on('message', message => {
     }
 
     if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
+        cooldowns.set(command.name, new Discord.Collection())
     }
 
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
+    const now = Date.now()
+    const timestamps = cooldowns.get(command.name)
+    const cooldownAmount = (command.cooldown || 3) * 1000
 
     if (!timestamps.has(message.author.id)) {
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        timestamps.set(message.author.id, now)
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
     }
     else {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount
 
         if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.author.send(`Merci d'attendre encore ${timeLeft.toFixed(1)} seconde(s) avant de réutiliser la commande \`${command.name}\`.`);
+            const timeLeft = (expirationTime - now) / 1000
+            return message.react('🕐')
         }
 
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        timestamps.set(message.author.id, now)
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
     }
 
     try {
