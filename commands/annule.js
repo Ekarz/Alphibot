d0dfc12b02aa1741be122cf7b65f25face28fd9e
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-27 21:58:21 +0200

    clarify annule description

--- commands/annule.js
+++ commands/annule.js
@@ -7,7 +7,8 @@ module.exports = {
     usage: "[groupe de jours]\nExemple : `!annule mardi vendredi`",
     guildOnly: true,
     raiderOnly: true,
-    description: "Annule une dispo précédemment envoyée pour l'organisation du raid.",
+    description: "Annule une dispo précédemment envoyée pour l'organisation du raid. " +
+        "Même en cas d'échec, les jours écrits correctement seront traités par le bot.",
     execute(message, args) {
         let isDeleted = true
         for (day of args) {
