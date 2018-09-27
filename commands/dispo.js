22a9753a3ae7f41235a389715415348e3e98de7c
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-27 20:04:59 +0200

    dispo command can now be used only once for all availabilities

--- commands/dispo.js
+++ commands/dispo.js
@@ -1,31 +1,50 @@
 const { raid } = require('./raid')
 const { days } = require('../config.json')
 
+const hourRegex = /^([01]?[0-9]|2[0-3])[Hh:]([0-5][0-9])?$/
+const shortcuts = /^(week|all|weekend|we)$/
+const week = days.slice(0, 4).concat(days.slice(-1).pop())
+const weekend = days.slice().splice(4, 2)
+
 module.exports = {
     name: 'dispo',
     args: true,
-    usage: '[date] [time] | Exemple : jeudi 21h',
+    usage: "[groupe de jours] [heure unique]\nCet usage peut être utilisé plusieurs fois dans une seule commande.\n" +
+        "*Exemple :* lundi jeudi 21h mardi 20h\nIl est possible d'utiliser week, weekend ou all comme raccourci pour les jours.",
     guildOnly: true,
     raiderOnly: true,
-    description: 'Donne une dispo pour l\'organisation du raid.',
+    description: "Envoie une dispo pour l'organisation du raid. En cas d'échec, il est possible qu'une partie des paramètres aient été traités quand même ; à vérifier avec un `!recap`.",
     execute(message, args) {
-        let OK = true
-        for (let i = 0; i < args.length; i += 2) {
-            const playerAdded = addPlayer(message.author, args[i], args [i+1])
-            if (!playerAdded) {
-                message.reply(`La date "${args[i]} ${args[i+1]}" est mauvaise, mais j'ai pu traiter les dates d'avant s'il y en avait.`)
-                OK = false
-                break
-            }
-        }
-        message.react(OK ? '✅' : '🚫')
+        const playerAdded = addPlayer(message.author, args)
+        message.react(playerAdded ? '✅' : '🚫')
     },
 }
 
-const addPlayer = (playerName, day, hour) => {
-    const index = days.indexOf(day.toLowerCase())
-    if (index >= 0 && hour.match(/^([01]?[0-9]|2[0-3])[Hh:]([0-5][0-9])?$/)) {
-        raid[index].players[playerName] = hour.toLowerCase().replace(':', 'h')
-        return true
-    } else return false
+const addPlayer = (playerName, args) => {
+    let groupOfDays = []
+    for (arg of args) {
+        const index = days.indexOf(arg.toLowerCase())
+        if (index >= 0) groupOfDays.push(arg.toLowerCase())
+        else if (arg.match(shortcuts)) {
+            const restOfArgs = args.slice(args.indexOf(arg) + 1)
+            switch (arg.toLowerCase()) {
+                case 'week':
+                    return addPlayer(playerName, week.concat(restOfArgs))
+                case 'all':
+                    return addPlayer(playerName, days.concat(restOfArgs))
+                case 'weekend':
+                case 'we':
+                    return addPlayer(playerName, weekend.concat(restOfArgs))
+            }
+        }
+        else if (arg.match(hourRegex)) {
+            if (!groupOfDays.length) return false
+            for (day of groupOfDays) {
+                raid[days.indexOf(day)].players[playerName] = arg.toLowerCase().replace(':', 'h')
+            }
+            groupOfDays.length = 0
+        }
+        else return false
+    }
+    return (args.length >= 2 && !!args[args.length - 1].match(hourRegex))
 }
