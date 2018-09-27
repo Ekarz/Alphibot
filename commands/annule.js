63649ea7a144d7f12fc693d4b8142a8f51431f68
Antoine Robillard <antoine.robillard@noos.fr>
2018-09-27 21:52:55 +0200

    add annule command

---
+++ commands/annule.js
@@ -0,0 +1,20 @@
+const { raid } = require('./raid')
+const { days } = require('../config.json')
+
+module.exports = {
+    name: 'annule',
+    args: true,
+    usage: "[groupe de jours]\nExemple : `!annule mardi vendredi`",
+    guildOnly: true,
+    raiderOnly: true,
+    description: "Annule une dispo précédemment envoyée pour l'organisation du raid.",
+    execute(message, args) {
+        let isDeleted = true
+        for (day of args) {
+            const index = days.indexOf(day)
+            if (index >= 0) delete raid[index].players[message.author]
+            else isDeleted = false
+        }
+        message.react(isDeleted ? '✅' : '🚫')
+    },
+}
